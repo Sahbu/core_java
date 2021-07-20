3eadb439669fba37fc09117def007ffc8d7fcee0
Sahbu <https://github.com/Sahbu/core_java.git>
2021-07-19 21:59:28 -0700

    git fill commited

---
+++ Fish.java
@@ -0,0 +1,10 @@
+package constructor.inheritance;
+
+public class Fish extends Animal{
+
+	public Fish(String name, int brain, int body, int size, int weight) {
+		super(name, brain, body, size, weight);
+		
+	}
+
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+package constructor.inheritance;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		Dog rotwiler=new Dog("Rotwiler", 2, 28, 2, 4, 1, "straight and coarse");
+		
+		//rotwiler.eat();
+		rotwiler.walk();
+		rotwiler.run();
+
+	}
+
+}
